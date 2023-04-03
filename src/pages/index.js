a10007d2fcd1081f370a36686f73f202876d039d
cmelonijs <carlomeloni63@gmail.com>
2023-04-03 12:31:40 +0200

    feat: using a store inside a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useSelector, useDispatch } from "react-redux";
+import { addDate } from '../datesSlice';
+import { store } from '../store';
 
 export default function Home() {
+  const dates = useSelector((state) => state.dates.value);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addDate())
+  }
+
   return (
     <>
-      
+      <button onClick={handleClick}>Cattura il momento</button>
+      {dates.forEach((date) => (
+        <h1>{date}</h1>
+      ))}
     </>
   )
 }
